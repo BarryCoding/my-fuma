30326d197c6f303a41f6bc5780401fc333d3c918
Springer <zxllks@yahoo.com>
2025-08-24 10:38:59 +0800

    simple setup: section fumadocs and section reader

---
+++ src/lib/sidebar.ts
@@ -0,0 +1,14 @@
+export const sidebar = {
+  tabs: [
+    {
+      title: 'As a Reader',
+      url: '/docs/reader',
+      icon: '📚',
+    },
+    {
+      title: 'Fumadocs',
+      url: '/docs/fumadocs',
+      icon: '🐼',
+    },
+  ],
+}
